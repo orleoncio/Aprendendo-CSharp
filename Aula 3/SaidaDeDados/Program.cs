87bed38ae04eacf50f9412f6ade005f222fef197
orleoncio <orleonciofilho@hotmail.com>
2024-02-05 21:31:37 -0300

    Terceiro programa do curso de C#

---
+++ Aula 3/SaidaDeDados/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Diferença entre Write.Line e Write
+            //O Console.WriteLine faz com que o output seja direto na linha abaixo diferente do Console.Write
+            Console.Write("Bom Dia!");
+            Console.Write("Boa Noite!");
+            Console.WriteLine("Bom Dia!");
+            Console.WriteLine("Boa Noite!");
+            
+            //Tipos de Concatenação
+            int idade = 30;
+            float saldo = 10.76538f;
+            string nome = "Maria";
+
+            //Placeholders
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2} reais", nome, idade, saldo);
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais", nome, idade, saldo);
+
+            //Interpolação
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais");
+
+            //Concatenação
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo + " reais");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2") + " reais");
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais");
+        }
+    }
+}
